35891d92e50eec39a7dbdbd5876a6195752ff2dc
theocatusanu-ui <theocatusanu@gmail.com>
2026-02-16 20:09:02 -0500

    pages fixed

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Vidal-Brothers-Auto-Repair/',
+    base: '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
